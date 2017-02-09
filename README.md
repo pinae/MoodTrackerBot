0c1271437a391a598241ee5aad634d68142f7483
pinae <jonny@pinae.net>
2017-02-09 18:34:05 +0100

    A basic mood tracking bot

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # MoodTrackerBot
-Telegram bot who asks about the current mood and saves the answers for further analysis.
+Telegram bot who asks about the current mood and saves the answers for 
+further analysis. In Telegram you find this bot as `@mood_tracker_bot`.
+
+## Usage
+Start chatting with the bot. It will interpret every Message you send 
+as a mood and save it. After that it asks if this feeling is good or 
+bad. This information is saved as a numerical value.
+
+If you chat `/start` or `Hi` and it will ask you every hour how you 
+feel. You can stop that with `/stop` or `Stop`.
+
+## Installation
+I recommend a virtualenv:
+```shell
+pyvenv env
+source env/bin/activate
+```
+The dependencies can be installed via pip:
+```shell
+pip install -r requirements.txt
+```
+Just run `bot.py` to connect to telegram and answer to messages.
+
+## Forking
+If you fork this bot you have to use your own API key. You get one by 
+chatting with the `@BotFather`. To save it create a python file
+ `apikey.py` which defines the variable `API_KEY`:
+ ```Python
+ API_KEY = <your api key goes here>
+ ```
+ Without this file and a key the code will not work.
---
+++ bot.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function, unicode_literals
+import telepot
+from apikey import API_KEY
+from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
+from time import sleep
+from time import time as utc_time
+from datetime import datetime, time
+import re
+
+
+def write_users(user_list):
+    with open("users.txt", 'w') as users_file:
+        users_file.writelines([str(uid) for uid in user_list])
+
+
+def get_users():
+    user_list = []
+    try:
+        with open("users.txt", 'r') as users_file:
+            for line in users_file.readlines():
+                user_list.append(int(line))
+    except FileNotFoundError:
+        pass
+    return user_list
+
+
+def append_quality(number, user_id, timestamp):
+    with open("mood_data.csv", 'r') as data_file:
+        newest_line = data_file.readlines()[-1]
+    if re.match(r'\d+, \d+, [^,]+, -?\d', newest_line):
+        mood = re.match('\d+, \d+, ([^,]+), -?\d+', newest_line).groups()[0]
+        with open("mood_data.csv", 'a') as data_file:
+            data_file.write("\n" + ", ".join([str(user_id), str(timestamp), mood, str(number)]))
+    else:
+        with open("mood_data.csv", 'a') as data_file:
+            data_file.write(", " + str(number))
+
+
+def on_chat_message(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    if content_type == 'text':
+        user_id = msg['chat']['id']
+        if msg['text'] in ["/start", "Hallo", "Hi", "Start"]:
+            if user_id not in users:
+                users.append(user_id)
+                write_users(users)
+            bot.sendMessage(user_id, "In einem Wort: Wie fühlst du dich gerade?")
+        elif msg['text'] in ["/stop", "Hör auf", "Stop", "Ende"]:
+            users.remove(user_id)
+            write_users(users)
+            bot.sendMessage(user_id, "Ich frage dich ab jetzt nicht mehr.")
+        else:  # It must be a mood
+            with open("mood_data.csv", 'a') as data_file:
+                data_file.write("\n" + ", ".join([str(user_id), str(msg['date']), "".join(msg['text'].split(','))]))
+            keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                           [InlineKeyboardButton(text='mies', callback_data='-2'),
+                            InlineKeyboardButton(text='schlecht', callback_data='-1'),
+                            InlineKeyboardButton(text='neutral', callback_data='0'),
+                            InlineKeyboardButton(text='gut', callback_data='1'),
+                            InlineKeyboardButton(text='super', callback_data='2')],
+                       ])
+            bot.sendMessage(chat_id, 'Wie gut fühlt sich das an?', reply_markup=keyboard)
+
+
+def on_callback_query(msg):
+    query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
+    append_quality(query_data, from_id, int(utc_time()))
+    bot.answerCallbackQuery(query_id, text='OK, gespeichert.')
+
+
+bot = telepot.Bot(API_KEY)
+users = get_users()
+try:
+    bot.message_loop({'chat': on_chat_message,
+                      'callback_query': on_callback_query})
+    print("Bot started ...")
+    while True:
+        if time(23, 30) > datetime.now().time() > time(8, 30):
+            for user in users:
+                bot.sendMessage(user, "In einem Wort: Wie fühlst du dich gerade?")
+        sleep(60*60)
+except KeyboardInterrupt:
+    pass
