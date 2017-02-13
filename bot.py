17ce5775d651851bb2fa56f3ec16afb24178e6c0
pinae <jonny@pinae.net>
2017-02-13 17:53:21 +0100

    More robust to errors while answering.

--- bot.py
+++ bot.py
@@ -39,33 +39,36 @@ def append_quality(number, user_id, timestamp):
 
 
 def on_chat_message(msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
-    if content_type == 'text':
-        user_id = msg['chat']['id']
-        if msg['text'] in ["/start", "/start start", "Hallo", "Hi", "Start"]:
-            if user_id not in users:
-                users.append(user_id)
+    try:
+        content_type, chat_type, chat_id = telepot.glance(msg)
+        if content_type == 'text':
+            user_id = msg['chat']['id']
+            if msg['text'] in ["/start", "/start start", "Hallo", "Hi", "Start"]:
+                if user_id not in users:
+                    users.append(user_id)
+                    write_users(users)
+                bot.sendMessage(user_id, "In einem Wort: Wie fühlst du dich gerade?")
+            elif msg['text'] in ["/stop", "Hör auf", "Stop", "Ende"]:
+                users.remove(user_id)
                 write_users(users)
-            bot.sendMessage(user_id, "In einem Wort: Wie fühlst du dich gerade?")
-        elif msg['text'] in ["/stop", "Hör auf", "Stop", "Ende"]:
-            users.remove(user_id)
-            write_users(users)
-            bot.sendMessage(user_id, "Ich frage dich ab jetzt nicht mehr.")
-        elif msg['text'].startswith("/"):
-            bot.sendMessage(user_id, "Mit dem Befehl `" + msg['text'] + "` kann ich leider nichts anfangen.")
-            bot.sendMessage(user_id, "Ich verstehe nur /start und /stop. Bei allen anderen Nachrichten gehe ich " +
-                            "davon aus, dass es ein Gefühl ist.")
-        else:  # It must be a mood
-            with open("mood_data.csv", 'a') as data_file:
-                data_file.write("\n" + ", ".join([str(user_id), str(msg['date']), "".join(msg['text'].split(','))]))
-            keyboard = InlineKeyboardMarkup(inline_keyboard=[
-                           [InlineKeyboardButton(text='mies', callback_data='-2'),
-                            InlineKeyboardButton(text='schlecht', callback_data='-1'),
-                            InlineKeyboardButton(text='neutral', callback_data='0'),
-                            InlineKeyboardButton(text='gut', callback_data='1'),
-                            InlineKeyboardButton(text='super', callback_data='2')],
-                       ])
-            bot.sendMessage(chat_id, 'Wie gut fühlt sich das an?', reply_markup=keyboard)
+                bot.sendMessage(user_id, "Ich frage dich ab jetzt nicht mehr.")
+            elif msg['text'].startswith("/"):
+                bot.sendMessage(user_id, "Mit dem Befehl `" + msg['text'] + "` kann ich leider nichts anfangen.")
+                bot.sendMessage(user_id, "Ich verstehe nur /start und /stop. Bei allen anderen Nachrichten gehe ich " +
+                                "davon aus, dass es ein Gefühl ist.")
+            else:  # It must be a mood
+                with open("mood_data.csv", 'a') as data_file:
+                    data_file.write("\n" + ", ".join([str(user_id), str(msg['date']), "".join(msg['text'].split(','))]))
+                keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                               [InlineKeyboardButton(text='mies', callback_data='-2'),
+                                InlineKeyboardButton(text='schlecht', callback_data='-1'),
+                                InlineKeyboardButton(text='neutral', callback_data='0'),
+                                InlineKeyboardButton(text='gut', callback_data='1'),
+                                InlineKeyboardButton(text='super', callback_data='2')],
+                           ])
+                bot.sendMessage(chat_id, 'Wie gut fühlt sich das an?', reply_markup=keyboard)
+    except telepot.exception.BotWasBlockedError:
+        pass
 
 
 def on_callback_query(msg):
