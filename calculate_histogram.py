ba3ab21781795bfa81eb19edf91240a8b889043e
pinae <jonny@pinae.net>
2017-07-16 11:17:39 +0200

    Some additional statistics

--- calculate_histogram.py
+++ calculate_histogram.py
@@ -6,6 +6,8 @@ import re
 
 def calculate_histogram(user_id):
     histogram = {}
+    mood_sum = 0.0
+    mood_count = 0
     with open("mood_data.csv", 'r') as data_file:
         for data_line in data_file.readlines():
             if re.match(r'\d+, \d+, [^,]+, -?\d', data_line):
@@ -13,6 +15,8 @@ def calculate_histogram(user_id):
                 mood = re.match('\d+, \d+, ([^,]+), -?\d+', data_line).groups()[0]
                 value = int(re.match('\d+, \d+, ([^,]+), (-?\d+)', data_line).groups()[1])
                 if uid == user_id:
+                    mood_sum += value
+                    mood_count += 1
                     if mood.lower() in histogram:
                         histogram[mood.lower()] = (histogram[mood.lower()][0] + value, histogram[mood.lower()][1] + 1)
                     else:
@@ -24,7 +28,9 @@ def calculate_histogram(user_id):
         if len(answers[-1]) >= 10:
             answers.append([])
         answers[-1].append(mood + ": " + str(value) + " (" + str(histogram[mood.lower()][1]) + ")")
-    return ["```\n" + "\n".join(answer) + "\n```" for answer in answers]
+    return ["```\n" + "\n".join(answer) + "\n```" for answer in answers] + \
+           ["Insgesamt " + str(len(histogram)) + " Gefühle.",
+            "Die durchschnittliche Gefühlslage war: " + str(mood_sum/mood_count)]
 
 if __name__ == "__main__":
     print(calculate_histogram(9700336))
